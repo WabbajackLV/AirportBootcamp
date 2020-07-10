7938970f9ecc39a825268728a3bf3c63a73089ce
marinarediko <ma.rediko@gmail.com>
2020-07-10 16:48:09 +0300

    feat(Admin) added getters setters construstors and more

--- AirportBootC/src/main/java/com/example/demo/models/Admin.java
+++ AirportBootC/src/main/java/com/example/demo/models/Admin.java
@@ -1,17 +1,35 @@
 package com.example.demo.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor 
+
 @Entity
 @Table(name="AdminTable")
 public class Admin extends Person {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="A_ID")
+	private long a_ID;
 	
+	Admin()
+	{
+		
+	}
+
+	public long getA_ID() {
+		return a_ID;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [a_ID=" + a_ID + "]";
+	}
 	
 }
