7303f3cdc75435f71376cf4f3cc6d9c5c9ab30d9
WabbajackLV <bleachlv@inbox.lv>
2020-07-10 15:37:40 +0300

    Created RegisteredUser class. Will need further editing

---
+++ AirportBootC/src/main/java/com/example/demo/models/RegisteredUser.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@Table(name = "RegisteredUserTable")
+@Entity
+public class RegisteredUser extends Person{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(AccessLevel.NONE)
+	@Column(name = "RU_ID" )
+	private int ru_id;
+	
+	@Min(0)
+	@Max(10000)
+	@Column(name = "Points")
+	private int points;
+	
+	@Column(name = "AllFlights")
+	private ArrayList<BoardingPass> allFlights;
+}
