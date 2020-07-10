a9fcbff96e6477ea1e34439101080b2b1effec5d
camelCast <casuallearner@protonmail.com>
2020-07-10 15:01:32 +0300

    New class for airport

---
+++ AirportBootC/src/main/java/com/example/demo/models/Airport.java
@@ -0,0 +1,44 @@
+package com.example.demo.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.example.demo.enums.Cities;
+import com.example.demo.enums.Countries;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "AirportTable")
+@Getter @Setter @NoArgsConstructor
+public class Airport {
+	
+	
+	public Countries country;
+	public Cities city;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(AccessLevel.NONE)
+	@Column(name = "A_ID" )
+	public int a_ID;
+	public String code;
+	
+
+	public Airport(Countries country, Cities city, int id, String code) {
+		super();
+		this.country = country;
+		this.city = city;
+		this.a_ID = id;
+		this.code = code;
+	}
+	
+	
+}
