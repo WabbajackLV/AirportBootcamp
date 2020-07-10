7a51dd96e3c9408da22d8fc04333932abb3bdbd8
camelCast <casuallearner@protonmail.com>
2020-07-10 14:42:47 +0300

    Countries and Cities enums

--- AirportBootC/src/main/java/com/example/demo/enums/Cities.java
+++ AirportBootC/src/main/java/com/example/demo/enums/Cities.java
@@ -2,4 +2,6 @@ package com.example.demo.enums;
 
 public enum Cities {
 
+	Riga, Tallinn, Vilnius, Helsinki, Stockholm, Oslo, Moscow, Minks, Warsaw, Kiyv, Berlin, Paris, Rome, Madrid, Lisbon, Brussels, Amsterdam, Bucharest, Sofia, Chisinau,
+	Athens, Istanbul, Tirana, Saraevo, Podgorica, Zagreb, Budapest, Vienna, Prague, Bratislava, Ljubljana, Zurich, Vaduz, Reykjavik, London, Dublin, Copenhagen, Sealand
 }
