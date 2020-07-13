3f485c3802f517ea8b8bfb75aa725ee39d84f3f7
marinarediko <ma.rediko@gmail.com>
2020-07-13 11:23:17 +0300

    fixed(IFlightRepo)

--- AirportBootC/src/main/java/com/example/demo/repos/IFlightRepo.java
+++ AirportBootC/src/main/java/com/example/demo/repos/IFlightRepo.java
@@ -1,14 +1,13 @@
 package com.example.demo.repos;
 
-import java.util.ArrayList;
+
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.models.Flight;
-import com.example.demo.models.RegisteredUser;
 
 public interface IFlightRepo extends CrudRepository<Flight, Integer>{
 
-	ArrayList<Flight> findByRegisteredUser(RegisteredUser regU);
+	
 
 }
